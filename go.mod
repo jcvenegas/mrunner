30ca84aede8181555adf0172699beff8a03d6fbb
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2020-09-23 21:08:46 +0000

    main: Add payload struct to use as file
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module mrunner
 
 go 1.13
 
-require github.com/codeskyblue/go-sh v0.0.0-20200712050446-30169cf553fe
+require (
+	github.com/codeskyblue/go-sh v0.0.0-20200712050446-30169cf553fe
+	gopkg.in/yaml.v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path"
 	"time"
@@ -9,21 +10,25 @@ import (
 	// TODO: Should not import entities
 	dtests "mrunner/entities/tests/docker"
 	"mrunner/usecases/tests"
+
+	"gopkg.in/yaml.v2"
 )
 
-func main() {
+type ContainerWorkload struct {
+	Name           string
+	Command        string
+	Exec           string
+	DockerFilePath string
+	TimeoutMinutes int
+}
 
-	kc := tests.Configs{
-		Runtimes: []string{"kata-clh", "kata-qemu"},
-		HypervisorConfigs: tests.HypervisorConfigs{
-			CacheTypes:      []string{"always"},
-			CacheSizesBytes: []int{1024},
-			VirtiofsdArgs:   []string{""},
-			KernelPaths: []string{
-				"/opt/kata/share/kata-containers/vmlinux-kata-v5.6-april-09-2020-88-virtiofs",
-			},
-		},
-	}
+type TestFile struct {
+	ContainerEngine   string
+	RuntimeConfigs    tests.Configs
+	ContainerWorkload ContainerWorkload
+}
+
+func main() {
 
 	fioCmd := "fio"
 	fioCmd += " --direct=1"
@@ -42,25 +47,53 @@ func main() {
 
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
 	}
 	dockerFilePath := path.Join(wd, "/workloads/fio/dockerfile/Dockerfile")
 
+	testFile := TestFile{
+		ContainerEngine: "docker",
+		RuntimeConfigs: tests.Configs{
+			Runtimes: []string{"kata-clh", "kata-qemu"},
+			HypervisorConfigs: tests.HypervisorConfigs{
+				CacheTypes:      []string{"always"},
+				CacheSizesBytes: []int{1024},
+				VirtiofsdArgs:   []string{""},
+				KernelPaths: []string{
+					"/opt/kata/share/kata-containers/vmlinux-kata-v5.6-april-09-2020-88-virtiofs",
+				},
+			},
+		},
+		ContainerWorkload: ContainerWorkload{
+			Name:           "large-files-4gb",
+			Command:        "",
+			Exec:           fioCmd,
+			DockerFilePath: dockerFilePath,
+			TimeoutMinutes: 10,
+		},
+	}
+	d, err := yaml.Marshal(&testFile)
+	err = ioutil.WriteFile("workloads.yaml", d, 0644)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+
 	t := dtests.DockerTest{
-		Name:           "large-files-4gb",
-		Command:        "",
-		Exec:           fioCmd,
-		DockerFilePath: dockerFilePath,
-		Timeout:        20 * time.Minute,
+		Name:           testFile.ContainerWorkload.Name,
+		Command:        testFile.ContainerWorkload.Command,
+		Exec:           testFile.ContainerWorkload.Exec,
+		DockerFilePath: testFile.ContainerWorkload.DockerFilePath,
+		Timeout:        time.Duration(testFile.ContainerWorkload.TimeoutMinutes) * time.Minute,
 	}
 
 	v := dtests.TestWorkDirVolume{ContainerShare: "/output"}
 	t.AddVolume(v)
 
-	fmt.Printf("Running: %#v\n", kc)
-
-	rs, err := tests.RunTestForKataConfigs(&t, kc)
+	rs, err := tests.RunTestForKataConfigs(&t, testFile.RuntimeConfigs)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
 	}
 
 	for _, r := range rs {
