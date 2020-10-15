647bdbe3414fb19faab3319442cdd2fe2cfe20e5
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@inte.com>
2020-10-15 15:51:34 +0000

    scripts: bash: fixes
    more fixes
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@inte.com>

--- scripts/bash_workloads/fio_4g.sh
+++ scripts/bash_workloads/fio_4g.sh
@@ -15,16 +15,17 @@ docker rm -f large-files-4gb
 
 # Running kata-runtime
 docker build -f Dockerfile -t large-files-4gb .
+/usr/local/bin/kata-runtime kata-env
 docker run -dti --runtime kata-runtime  --name large-files-4gb large-files-4gb
 sudo bash -c 'echo 3 > /proc/sys/vm/drop_caches'
 docker exec -i large-files-4gb fio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 
 docker rm -f large-files-4gb
 
 # Running kata-qemu-virtiofs
-sudo crudini --set --existing /opt/kata/share/defaults/kata-containers/configuration-qemu-virtiofs.toml hypervisor.qemu virtio_fs_cache "auto"
+sudo crudini --set --existing /opt/kata/share/defaults/kata-containers/configuration-qemu-virtiofs.toml hypervisor.qemu virtio_fs_cache '"auto"'
 sudo crudini --set --existing /opt/kata/share/defaults/kata-containers/configuration-qemu-virtiofs.toml hypervisor.qemu virtio_fs_cache_size 0
 sudo crudini --set --existing /opt/kata/share/defaults/kata-containers/configuration-qemu-virtiofs.toml hypervisor.qemu virtio_fs_extra_args []
-sudo crudini --set --existing /opt/kata/share/defaults/kata-containers/configuration-qemu-virtiofs.toml hypervisor.qemu kernel "/opt/kata/share/kata-containers/vmlinux.container"
+sudo crudini --set --existing /opt/kata/share/defaults/kata-containers/configuration-qemu-virtiofs.toml hypervisor.qemu kernel '"/opt/kata/share/kata-containers/vmlinux.container"'
 /opt/kata/bin/kata-qemu-virtiofs kata-env
 docker build -f Dockerfile -t large-files-4gb .
 docker run -dti --runtime kata-qemu-virtiofs  --name large-files-4gb large-files-4gb
@@ -34,6 +35,7 @@ docker rm -f large-files-4gb
 
 # Running kata-qemu
 docker build -f Dockerfile -t large-files-4gb .
+/opt/kata/bin/kata-qemu kata-env
 docker run -dti --runtime kata-qemu  --name large-files-4gb large-files-4gb
 sudo bash -c 'echo 3 > /proc/sys/vm/drop_caches'
 docker exec -i large-files-4gb fio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 
