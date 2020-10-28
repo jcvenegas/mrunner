4b8b3b544c536fe865e829f0ef355653c4ef25c0
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2020-10-28 20:16:09 +0000

    reduce: test size
    using small files can keep gettings results
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- scripts/bash_workloads/fio_4g.sh
+++ scripts/bash_workloads/fio_4g.sh
@@ -2,41 +2,32 @@
 
 set -x
 set -e
-git clone https://github.com/jcvenegas/mrunner.git || true
-cd mrunner/workloads/fio/dockerfile
+
+script_name=${0##*/}
+script_dir=$(dirname "$(readlink -f "$0")")
+
+cd "${script_dir}/../../workloads/fio/dockerfile/"
 
 docker rm -f large-files-4gb || true
-# Running runc
-docker build -f Dockerfile -t large-files-4gb .
-docker run -dti --runtime runc --name large-files-4gb large-files-4gb
-sudo bash -c 'echo 3 > /proc/sys/vm/drop_caches'
-docker exec -i large-files-4gb fio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 
-docker rm -f large-files-4gb
-
-# Running kata-runtime
-docker build -f Dockerfile -t large-files-4gb .
-/usr/local/bin/kata-runtime kata-env
-docker run -dti --runtime kata-runtime  --name large-files-4gb large-files-4gb
-sudo bash -c 'echo 3 > /proc/sys/vm/drop_caches'
-docker exec -i large-files-4gb fio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 
-docker rm -f large-files-4gb
-
 # Running kata-qemu-virtiofs
+# Defaults for virtiofs
 sudo crudini --set --existing /opt/kata/share/defaults/kata-containers/configuration-qemu-virtiofs.toml hypervisor.qemu virtio_fs_cache '"auto"'
 sudo crudini --set --existing /opt/kata/share/defaults/kata-containers/configuration-qemu-virtiofs.toml hypervisor.qemu virtio_fs_cache_size 0
 sudo crudini --set --existing /opt/kata/share/defaults/kata-containers/configuration-qemu-virtiofs.toml hypervisor.qemu virtio_fs_extra_args []
 sudo crudini --set --existing /opt/kata/share/defaults/kata-containers/configuration-qemu-virtiofs.toml hypervisor.qemu kernel '"/opt/kata/share/kata-containers/vmlinux.container"'
 /opt/kata/bin/kata-qemu-virtiofs kata-env
+echo "case: kata-qemu-virtiofs"
 docker build -f Dockerfile -t large-files-4gb .
 docker run -dti --runtime kata-qemu-virtiofs  --name large-files-4gb large-files-4gb
 sudo bash -c 'echo 3 > /proc/sys/vm/drop_caches'
-docker exec -i large-files-4gb fio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 
+docker exec -i large-files-4gb fio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=300M --readwrite=randrw --rwmixread=75
 docker rm -f large-files-4gb
 
 # Running kata-qemu
+echo "case: kata-qemu-9pfs"
 docker build -f Dockerfile -t large-files-4gb .
 /opt/kata/bin/kata-qemu kata-env
 docker run -dti --runtime kata-qemu  --name large-files-4gb large-files-4gb
 sudo bash -c 'echo 3 > /proc/sys/vm/drop_caches'
-docker exec -i large-files-4gb fio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 
+docker exec -i large-files-4gb fio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=300M --readwrite=randrw --rwmixread=75
 docker rm -f large-files-4gb
